6b49b758b14792e0fe3c4cfc361e046b93cc769c
Muhammet <tayyipakbulut@gmail.com>
2021-03-12 14:41:49 +0300

    Product and Category interfaces

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ 
+  category1 = {
+    categoryName: 'sa',
+  };
+  category2 = {
+    categoryName: 'sas',
+  };
+  category3 = {
+    categoryName: 'saa',
+  };
+  categories = [this.category1, this.category2, this.category3];
+   constructor() {}
+   ngOnInit(): void {}
 }
